69a48e648c02debbdfff285bea7a5e18d641c7ce
NaniIsJoey <37638087+Yunikuna-Joey@users.noreply.github.com>
2024-08-28 19:11:09 -0700

    [refactor]
    => refactor baseline code

--- app.py
+++ app.py
@@ -4,13 +4,14 @@ import google.generativeai as genai
 
 load_dotenv()
 
-
+#* Global registration variables 
 apiKey = os.getenv('API_TOKEN')
 genai.configure(api_key=apiKey)
+model = genai.GenerativeModel(os.getenv('API_MODEL'))
 
 
 #* Example request [refer back as necessary]
 def exampleClientRequest():
-    model = genai.GenerativeModel('gemini-1.5-flash')
-    response = model.generate_content("Write a story about a magic backpack.")
+    response = model.generate_content("What is the most popular food?")
     print(response.text)
+
