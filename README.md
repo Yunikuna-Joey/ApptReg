2e773d37b69fd1403a6ab4568238ddb1cc2269fb
Joey <37638087+Yunikuna-Joey@users.noreply.github.com>
2024-08-28 23:22:25 -0700

    [WIP]
    => setting up service account with google calendar authentication [wip]

--- README.md
+++ README.md
@@ -6,5 +6,6 @@
     - (Windows) to activate the environment project_name/Scripts/activate
 
 2) Packages used [mandatory]
-    - pip install -q -U google-generativeai
-    - pip install python-dotenv (personal pref. used to store keys)
+    - pip install -q -U google-generativeai [Gemini Model]
+    - pip install python-dotenv [personal pref. used to store keys]
+    - pip install --upgrade google-api-python-client google-auth-httplib2 google-auth-oauthlib [setting up access from service account to Google Calendars]
--- app.py
+++ app.py
@@ -1,3 +1,6 @@
+from google.oauth2.service_account import Credentials
+from googleapiclient.discovery import build 
+
 from dotenv import load_dotenv
 import os 
 import google.generativeai as genai
@@ -9,9 +12,19 @@ apiKey = os.getenv('API_TOKEN')
 genai.configure(api_key=apiKey)
 model = genai.GenerativeModel(os.getenv('API_MODEL'))
 
+#* Service account registration
+SCOPES = ['https://www.googleapis.com/auth/calendar']
+
 
 #* Example request [refer back as necessary]
 def exampleClientRequest():
     response = model.generate_content("What is the most popular food?")
     print(response.text)
+
+def example(): 
+    response = model.generate_content("Hello, I would like to schedule an appointment")
+    print(response.text)
+
+if __name__ == "__main__": 
+    example()
 
