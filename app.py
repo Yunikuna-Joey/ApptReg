cb8ae67c897cd3cba4c363ed4c03de3295a464f7
NaniIsJoey <37638087+Yunikuna-Joey@users.noreply.github.com>
2024-08-29 16:15:39 -0700

    [update]
    => successfully created a google calendar event [had to omit the attendee portion of the event object]

--- app.py
+++ app.py
@@ -14,6 +14,43 @@ model = genai.GenerativeModel(os.getenv('API_MODEL'))
 
 #* Service account registration
 SCOPES = ['https://www.googleapis.com/auth/calendar']
+creds = Credentials.from_service_account_file(os.getenv('SERVICE_ACCOUNT'), scopes=SCOPES)
+
+#* Initialize calendar API service
+calendarService = build('calendar', 'v3', credentials=creds)
+
+#* Specify the calendarId (which calendar) to modify | schedule appointments on 
+targetCalendarId = os.getenv('CALENDAR_ID')
+
+#* Example of creating an event on the company calendar 
+    # an Event object must have 
+    # Title | Summary
+    # Location 
+    # Description | Type of wash [interior, exterior, both]
+    # startTime | start
+    # endTime | end 
+    # customer | attendees
+eventObject = {
+    'summary': 'Company Meeting', 
+    'location': 'Office', 
+    'description': 'Discussing project updates', 
+    'start' : { 
+        'dateTime': '2024-09-01T10:00:00-07:00',
+        'timeZone': 'America/Los_Angeles',
+    }, 
+    'end': { 
+        'dateTime': '2024-09-01T11:00:00-07:00',
+        'timeZone': 'America/Los_Angeles',
+    }, 
+    # 'attendees': [
+    #     {'email': 'attendee1@example.com'},
+    #     {'email': 'attendee2@example.com'},
+    # ],
+}
+
+#* Insert the event into the company calendar
+# event = calendarService.events().insert(calendarId=targetCalendarId, body=eventObject).execute()
+# print('Event created: %s' % (event.get('htmlLink')))
 
 
 #* Example request [refer back as necessary]
@@ -26,5 +63,7 @@ def example():
     print(response.text)
 
 if __name__ == "__main__": 
-    example()
+    #* Insert the event into the company calendar
+    event = calendarService.events().insert(calendarId=targetCalendarId, body=eventObject).execute()
+    print('Event created: %s' % (event.get('htmlLink')))
 
