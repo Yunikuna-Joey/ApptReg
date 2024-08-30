12e40f3756ebda984eca4345cfddadb0a299cc28
NaniIsJoey <37638087+Yunikuna-Joey@users.noreply.github.com>
2024-08-29 18:02:07 -0700

    [update]
    => bot is able to send emails

--- app.py
+++ app.py
@@ -1,5 +1,12 @@
+# Loads service account credentials
 from google.oauth2.service_account import Credentials
+# Creates the connection to one of Google's product API (in our case, Google Calendar)
 from googleapiclient.discovery import build 
+
+#* Emailing purposes
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
 
 from dotenv import load_dotenv
 import os 
@@ -11,6 +18,49 @@ load_dotenv()
 apiKey = os.getenv('API_TOKEN')
 genai.configure(api_key=apiKey)
 model = genai.GenerativeModel(os.getenv('API_MODEL'))
+
+#* Email configuration
+smtp_server = os.getenv('EMAIL_SERVER')
+smtp_port = os.getenv('EMAIL_PORT')
+smtp_username = os.getenv('EMAIL_CRED')
+smtp_password = os.getenv('PASSWORD_CRED')
+
+# setup MIME 
+message = MIMEMultipart()
+message['From'] = smtp_username
+message['To'] = smtp_password 
+message['Subject'] = "Appointment Confirmation"
+
+body = """ 
+Hello insert_customer_name_here, 
+
+This message is a confirmation of your car detailing appointment. 
+
+Details: 
+- **Car**: Year, make, model 
+- **Location**: Address 
+- **Type of Cleaning**: Interior, exterior, both
+- **Start Time**: Format the time as Month Name, Day, Year @ 12 hour format time 
+- **End Time**: Format the time as Month Name, Day, Year @ 12 hour format time 
+
+Feel free to add this event in your Google Calendar automatically by clicking the .ics file.
+
+Best regards,
+Ten (Just an AutoBot)
+
+Do not reply back to this message, inbox is unmonitored.
+
+"""
+
+message.attach(MIMEText(body, 'plain'))
+
+# email server connection
+# server = smtplib.SMTP(smtp_server, smtp_port)
+# server.starttls()
+# server.login(smtp_username, smtp_password)
+# server.sendmail(smtp_username, os.getenv("TEST_USER"), message.as_string())
+# server.quit()
+
 
 #* Service account registration
 SCOPES = ['https://www.googleapis.com/auth/calendar']
@@ -31,15 +81,15 @@ targetCalendarId = os.getenv('CALENDAR_ID')
     # endTime | end 
     # customer | attendees
 eventObject = {
-    'summary': 'Company Meeting', 
-    'location': 'Office', 
-    'description': 'Discussing project updates', 
+    'summary': '2006 Acura RSX', 
+    'location': 'insert_address_here', 
+    'description': 'Interior & Exterior Cleaning', 
     'start' : { 
-        'dateTime': '2024-09-01T10:00:00-07:00',
+        'dateTime': '2024-09-02T10:00:00-07:00',
         'timeZone': 'America/Los_Angeles',
     }, 
     'end': { 
-        'dateTime': '2024-09-01T11:00:00-07:00',
+        'dateTime': '2024-09-02T11:00:00-07:00',
         'timeZone': 'America/Los_Angeles',
     }, 
     # 'attendees': [
@@ -67,3 +117,8 @@ if __name__ == "__main__":
     event = calendarService.events().insert(calendarId=targetCalendarId, body=eventObject).execute()
     print('Event created: %s' % (event.get('htmlLink')))
 
+    server = smtplib.SMTP(smtp_server, smtp_port)
+    server.starttls()
+    server.login(smtp_username, smtp_password)
+    server.sendmail(smtp_username, os.getenv("TEST_USER"), message.as_string())
+    server.quit()
