30dc843e9ad9c96996ab46939dd72336bd4ac403
Joey <37638087+Yunikuna-Joey@users.noreply.github.com>
2024-10-05 00:19:31 -0700

    [update]
    => refactored ngrok set up to helper function
    => local server seen by the public
    => need to look more into isolating requests only from meta api

--- webhook.py
+++ webhook.py
@@ -1,4 +1,5 @@
 from flask import Flask, request, jsonify 
+from webhookhelper import initializeNgrokService
 import ngrok
 
 from dotenv import load_dotenv
@@ -12,8 +13,8 @@ verifyToken = os.getenv('VERIFY_TOKEN')
 # the route is the url 
 # function is nothing special
 @app.route('/')
-def hello():
-    return "Hello, World! This is served via Ngrok with basic auth."
+def homePage():
+    return "Hello, World!"
 
 @app.route('/webhook', methods=['GET'])
 def verifyWebhook():
@@ -42,20 +43,6 @@ def verifyWebhook():
         return 'Verification failed', 403
     
 if __name__ == '__main__': 
-    # programatically set up the auth token for the user
-    ngrok.set_auth_token(os.getenv('NGROK_TOKEN'))
-    
-    # set up basic auth with your chosen values 
-    username = os.getenv('BAUTH_USER')
-    pw = os.getenv('BAUTH_PW')
-    
-    # link the local server from Flask (port #) to the domain, with the basic auth credentials set up
-    publicUrl = ngrok.connect(
-        addr=5000, 
-        domain=f"{os.getenv('NGROK_DOMAIN')}",
-        basic_auth=[f"{username}:{pw}"]
-    )
-
-    print(f" * Public URL: {publicUrl}")
+    initializeNgrokService()
 
     app.run(port=5000)
---
+++ webhookhelper.py
@@ -0,0 +1,40 @@
+from dotenv import load_dotenv
+
+import ngrok 
+import os 
+
+load_dotenv()
+
+
+def initializeNgrokService(): 
+    """
+    This helper function handles setting up the ngrok tunnel that allows the local server to be seen by the meta api
+    """
+    # previous iterations of intializing tunnel [refer as necessary]
+    # link the local server from Flask (port #) to the domain, with the basic auth credentials set up
+    # publicUrl = ngrok.connect(
+    #     addr=5000, 
+    #     domain=f"{os.getenv('NGROK_DOMAIN')}",
+    #     basic_auth=[f"{username}:{pw}"]
+    # )
+
+    # publicUrl = ngrok.connect(
+    #     addr=5000, 
+    #     domain=f"{os.getenv('NGROK_DOMAIN')}"
+    # )
+
+    # need to set auth token of who instantiated the tunnel
+    ngrok.set_auth_token(os.getenv('NGROK_TOKEN'))
+
+    # start the tunnel with these parameters
+    ngrokTunnel = ngrok.connect(
+        proto="http", 
+        addr=5000, 
+        domain=f"{os.getenv('NGROK_DOMAIN')}",
+        verify_webhook="FACEBOOK_GRAPH_API", 
+        verify_webhook_secret=os.getenv('APP_SECRET')
+    )
+
+    print(f"ngrok tunnel is running at: {ngrokTunnel.url}")
+
+    
