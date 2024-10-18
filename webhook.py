8bf3c6c0892a50a017b54fb2c0e655e7104db92c
NaniIsJoey <37638087+Yunikuna-Joey@users.noreply.github.com>
2024-10-18 13:47:27 -0700

    [update]
    => added another agent into the accesslist to perform message sending/receiving tasks/events
    => key into the correct events being sent from the api
    => handle sendMessage events, messageReceived events, and readMessage events

--- webhook.py
+++ webhook.py
@@ -11,7 +11,7 @@ load_dotenv()
 app = Flask(__name__)
 
 verifyToken = os.getenv('VERIFY_TOKEN')
-accessList = [os.getenv('USER_AGENT_FIELD'), os.getenv('WEBHOOK')]
+accessList = [os.getenv('USER_AGENT_FIELD'), os.getenv('WEBHOOK'), os.getenv('IG_MSG_AGENT')]
 
 # the route is the url 
 # function is nothing special
@@ -65,48 +65,67 @@ def processPostRequest():
         if getUserAgentHeader() not in accessList:
             return 'Verification failed', 401
 
-        else:
-            """ 
-            The webhook will listen for a Message Event from Meta API 
-            We extract the message content from the webhook request 
-            We extract the message sender ID for session management
+        """ 
+        The webhook will listen for a Message Event from Meta API 
+        We extract the message content from the webhook request 
+        We extract the message sender ID for session management
 
-            Pass it into our logic for attempting an appointment
-                => Option1: What if we grab the intent from the first message? 
-                    So if message has intent: proceed with logic 
-                    (issue: this is a while loop, so how do we keep chat persistence(?) 
-                    as we are receiving Message Events one at a time (one message at a time))
-                    
-                    If no intent: we just generate a response from the LLM 
-                 
-            """
-            # payload will have the information that we need to extract coming from meta api 
-            payload = request.get_json()
+        Pass it into our logic for attempting an appointment
+            => Option1: What if we grab the intent from the first message? 
+                So if message has intent: proceed with logic 
+                (issue: this is a while loop, so how do we keep chat persistence(?) 
+                as we are receiving Message Events one at a time (one message at a time))
+                
+                If no intent: we just generate a response from the LLM 
+                
+        """
+        # payload will have the information that we need to extract coming from meta api 
+        payload = request.get_json()
+        # print(f"This is the incoming payload received {payload}")
 
+        # this will help determine how to handle the payload request coming into the endpoint
+        payloadIdentifier = payload['entry'][0]['messaging'][0]             # so far we keep track of a message event and a message_read(app reading the message from the client)
+
+        # This will handle a normal message event 
+        if 'message' in payloadIdentifier and payloadIdentifier['sender']['id'] != os.getenv('INSTAGRAM_PAGE_ID'):          # we need a condition so that the bot doesn't attempt to process itself
             messageContent = extractMessageContentFromPayload(payload)
             senderId = extractSenderIdFromPayload(payload)
+
+            # print(f"This is the value of messageContent {messageContent}")
+            # print(f"This is the value of senderId {senderId}")
+            # print(f"This is the variable type of senderId {type(senderId)}")
 
             # implement the chatbot logic 
             responseMessageContent = additionScenario(senderId, messageContent)
             
-            status, requestResponse = sendMsg(responseMessageContent, senderId)
+            status, requestResponse = sendMsg(senderId, responseMessageContent)
 
-            print(f"Send message status: {status}, response: {requestResponse}")
-            
-            # print(f"[Second Call]: This is the incoming payload: {payload}")
-
-            # After processing the request, we need to return a 200 success code 
-            # return 'Message received', 200 
+            # print(f"Send message status: {status}, response: {requestResponse}")
+        
+        # This will handle a read message event
+        elif 'read' in payloadIdentifier:
             return jsonify({"status": "ok"}), 200
+        
+        # This will handle a message send event
+        elif 'is_echo' in payloadIdentifier['message']: 
+            return jsonify({"status": "ok"}), 200
+
+        # this should catch any other unhandled event that can be used for debugging
+        else: 
+            print("[Webhook_Endpoint]: This is an unhandled event type")
+            return jsonify({"status": "Unhandled event type"}), 400
+
+        # After processing the request, we need to return a 200 success code 
+        return jsonify({"status": "ok"}), 200
 
     except Exception as e:
         print(f"There was an error with processing the request {e}")
         return f"There was an error processing the request {e}", 403
 
 def sendMsg(userId, messageContent): 
-    # url = f"https://graph.facebook.com/v21.0/{os.getenv('INSTAGRAM_PAGE_ID')}/messages"
-    url = f"https://graph.instagram.com/v21.0/{os.getenv('INSTAGRAM_PAGE_ID')}/messages"
-
+    # url = f"https://graph.instagram.com/v21.0/{os.getenv('INSTAGRAM_PAGE_ID')}/messages"
+    # simplified, but should look into what the full url should be... 
+    url = f"https://graph.instagram.com/v21.0/me/messages"
 
     payload = { 
         'recipient': {'id': userId}, 
@@ -120,7 +139,7 @@ def sendMsg(userId, messageContent):
     }
 
     response = requests.post(url, json=payload, headers=headers)
-
+    # error that seems to be happening is that the message sent from the bot is being ran through essentially itself which shouldn't be happening
     print(f"Response Status: {response.status_code}, Response Text: {response.text}")
     return response.status_code, response.text
 
